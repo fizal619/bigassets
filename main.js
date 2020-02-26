3ea81131093740058202bebd4094c3847c761ccf
GNz11 <sshammasova@gmail.com>
2020-02-25 20:16:24 -0500

    solution added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 for (let i = 1; i < 800; i++) {
   fetch(`https://fizal.me/pokeapi/api/v2/id/${i}.json`)
     .then(function(r){
+      // need to load pokemons by their ids in order
       return r.json();
     })
     .then(function(data) {
